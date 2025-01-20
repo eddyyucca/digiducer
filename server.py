592958468eda5e06966f43416d8ff846ccf6c1d7
eddyyucca <eddyyucca@gmail.com>
2025-01-20 15:24:01 +0800

    update

--- server.py
+++ server.py
@@ -1,3 +1,39 @@
+# Author: Eddy Adha Saputra
+# GitHub: https://github.com/eddyyucca
+# Description: [Masukkan deskripsi singkat tentang program ini]
+# Realtime Data Dashboard with Flask
+# Dashboard ini adalah aplikasi real-time untuk memantau data dari perangkat 333D01, termasuk Acceleration, Velocity, dan Demodulation.
+
+# Fitur
+# Realtime Monitoring: Menampilkan data akselerasi, kecepatan, dan demodulasi secara real-time.
+# Flask Backend: Backend berbasis Python menggunakan Flask.
+# Responsive Dashboard: Antarmuka yang ramah pengguna dan responsif.
+
+
+if __name__ == "__main__":
+    print(r"""
+   
+  _____     _      _                                                                       
+ | ____| __| |  __| | _   _  _   _  _   _   ___  __ _                                      
+ |  _|  / _` | / _` || | | || | | || | | | / __|/ _` |                                     
+ | |___| (_| || (_| || |_| || |_| || |_| || (__| (_| |                                     
+ |_____|\__,_| \__,_| \__, | \__, | \__,_| \___|\__,_|                                     
+                      |___/  |___/                                                         
+  _____  _              ____            _                                                  
+ |_   _|| |__    ___   / ___| ___    __| |  ___                                            
+   | |  | '_ \  / _ \ | |    / _ \  / _` | / _ \                                           
+   | |  | | | ||  __/ | |___| (_) || (_| ||  __/                                           
+   |_|  |_| |_| \___|  \____|\___/  \__,_| \___|                                           
+                                        _          _    _                                _ 
+   ___  ___   _ __  _ __  _   _  _ __  | |_  ___  | |_ | |__    ___   ___   ___   _   _ | |
+  / __|/ _ \ | '__|| '__|| | | || '_ \ | __|/ __| | __|| '_ \  / _ \ / __| / _ \ | | | || |
+ | (__| (_) || |   | |   | |_| || |_) || |_ \__ \ | |_ | | | ||  __/ \__ \| (_) || |_| || |
+  \___|\___/ |_|   |_|    \__,_|| .__/  \__||___/  \__||_| |_| \___| |___/ \___/  \__,_||_|
+                                |_|                                                        
+
+    """)
+
+    
 from flask import Flask, jsonify, render_template
 import numpy as np
 import sounddevice as sd
